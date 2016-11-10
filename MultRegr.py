91bc1d2a316f5836d1fe85d4076e111fae3cb961
root <root@gorgona>
2016-11-10 14:33:49 -0500

    nuevos archivos

--- MultRegr.py
+++ MultRegr.py
@@ -13,15 +13,35 @@ import pandas as pd
 
 # importo datos de excel y paso valores al array arnum
 libro = openpyxl.load_workbook('compWC.xlsx')
-hoja = libro.get_sheet_by_name('h4')
-ar = pd.read_excel('compWC.xlsx','h4', header=0, index_col=None, na_values=['NA'])
+hoja = libro.get_sheet_by_name('h5')
+ar = pd.read_excel('compWC.xlsx','h5', header=0, index_col=None, na_values=['NA'])
 arnum=ar.values
 
-x = arnum[:,4:14].astype(float)
-y = arnum[:,1].astype(float)
+sizeBZ = 0
+sizeN = 0
 
-Fs,vals = f_regression(x, y, center=True)
+for i in arnum[:,0]:
+	if i == 'BZ':
+		sizeBZ +=1
+	else:
+		sizeN +=1
+print('BZ:')
+print (ar.columns[2],ar.columns[3],ar.columns[4],ar.columns[5])
 
-print(Fs)
-print(vals)
+xBZ = arnum[0:sizeBZ,2:6].astype(float)
+yBZ = arnum[0:sizeBZ,1].astype(float)
+FsBZ,valsBZ = f_regression(xBZ, yBZ, center=True)
 
+print(FsBZ)
+print(valsBZ)
+
+print ('N:')
+xN = arnum[sizeBZ:(sizeBZ+sizeN),2:6].astype(float)
+yN = arnum[sizeBZ:(sizeBZ+sizeN),1].astype(float)
+
+FsN,valsN = f_regression(xN, yN, center=True)
+
+print(FsN)
+print(valsN)
+
+# END
