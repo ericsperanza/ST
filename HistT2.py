2351325e0e50c421619ba0c710085673eb49c9c4
root <root@gorgona>
2017-03-07 22:03:58 -0500

    paso a B&W el histo

--- HistT2.py
+++ HistT2.py
@@ -8,6 +8,8 @@ from scipy import interpolate
 from scipy import signal
 import numpy as np
 import pandas as pd
+from matplotlib.patches import Rectangle
+
 
 # importo datos de excel y paso valores al array arnum
 libro = openpyxl.load_workbook('HistRatios.xlsx')
@@ -42,17 +44,17 @@ for a in range(1,18):
 	meandevN = (np.append(meandevN,[[np.mean(arnumdw[sizeBZ:(sizeBZ+sizeN),a])],[np.std(arnumdw[sizeBZ:(sizeBZ+sizeN),a],dtype=float,ddof=1)]], axis = 1))
 
 # creo el fondo de la figura
-fig=plt.figure(facecolor='white', figsize=(7,13.2))
+fig=plt.figure(facecolor='w', figsize=(7,14.2))
 
 #creo el histograma
 histo = fig.add_axes([0.18, 0.59, 0.8, 0.25])
 cols = np.arange(17) # ctdad de barras(= esteroles)
 ancho = 0.3
-eb = {'linewidth':2,'capsize':4,'ecolor':'r'}
-eb2 = {'linewidth':2,'capsize':4,'ecolor':'g'}
+eb = {'linewidth':2,'capsize':4,'ecolor':'k'}
+eb2 = {'linewidth':2,'capsize':4,'ecolor':'grey'}
 #eb3 = {'linewidth':2,'capsize':4,'ecolor':'brown'}
-rect1 = histo.bar(cols,meandevBZ[0,:],ancho, color = 'r', yerr=[np.zeros(17), meandevBZ[1,:]], error_kw=eb, linewidth=0, log=True)
-rect2 = histo.bar(cols+ancho,meandevN[0,:], ancho, color = 'g', yerr = [np.zeros(17),meandevN[1,:]], error_kw = eb2, linewidth=0, log=True)
+rect1 = histo.bar(cols,meandevBZ[0,:],ancho, color = 'k', yerr=[np.zeros(17), meandevBZ[1,:]], error_kw=eb, linewidth=0, log=True)
+rect2 = histo.bar(cols+ancho,meandevN[0,:], ancho, color = 'darkgrey', yerr = [np.zeros(17),meandevN[1,:]], error_kw = eb2, linewidth=0, log=True)
 #rect3 = histo.bar(cols+ancho*2,arnumfec[0,1:18], ancho, color = 'brown', yerr = [np.zeros(17),arnumfec[1,1:18]], error_kw = eb3, linewidth=0, log=True)
 # labels y ejes
 csfont = {'fontname':'Liberation Sans'}
@@ -72,14 +74,14 @@ plt.setp(histo.get_xticklabels(),visible=False)
 tortaBZ = fig.add_axes([0.21,0.77,0.265,0.265])
 tortaN = fig.add_axes([0.69,0.77,0.265,0.265])
 categ = ('Fecal','Phytosterols','Cholesterol','Others')
-colors = ['saddlebrown','limegreen','grey','dodgerblue']
-colorsN = ['limegreen','saddlebrown','dodgerblue','grey']
+colors = ['black','white','grey','lightgrey']
+colorsN = ['white','k','lightgrey','grey']
 BZ=[(np.mean(arnumpp[0:sizeBZ,19])),(np.mean(arnumpp[0:sizeBZ,20])),(np.mean(arnumpp[0:sizeBZ,14])),(np.mean(arnumpp[0:sizeBZ,21]))]
 print(BZ)
 N=[(np.mean(arnumpp[sizeBZ:(sizeBZ+sizeN),20])),(np.mean(arnumpp[sizeBZ:(sizeBZ+sizeN),19])),(np.mean(arnumpp[sizeBZ:(sizeBZ+sizeN),21])),(np.mean(arnumpp[sizeBZ:(sizeBZ+sizeN),14]))]
 print(N)
-tortaBZ.pie(BZ,startangle=40, colors=colors,wedgeprops={'linewidth':0})
-tortaN.pie(N,startangle=245, colors=colorsN,wedgeprops={'linewidth':0})
+tortaBZ.pie(BZ,startangle=40, colors=colors,wedgeprops={'linewidth':1})
+tortaN.pie(N,startangle=245, colors=colorsN,wedgeprops={'linewidth':1})
 tortaBZ.set_aspect('equal')
 tortaN.set_aspect('equal')
 # agrego los rotulos
@@ -93,14 +95,14 @@ tortaBZ.text(1.45,-0.14, r'Cholesterol', fontsize=18,**csfont)
 tortaBZ.arrow(0.96,0.00,0.4,0.00,head_width=0.02,width=0.01,head_length=0,clip_on=False,color='black',edgecolor='black')
 tortaBZ.arrow(3.15,0,0.65,0,head_width=0.02,width=0.01,head_length=0,clip_on=False,color='black',edgecolor='black')
 tortaBZ.text(1.4,-1.06, r'Phytosterols', fontsize=18,**csfont)
-tortaBZ.arrow(0.74,-0.6,0.6,-0.33,head_width=0.02,width=0.01,head_length=0,clip_on=False,color='black',edgecolor='black')
+tortaBZ.arrow(0.74,-0.65,0.6,-0.33,head_width=0.02,width=0.01,head_length=0,clip_on=False,color='black',edgecolor='black')
 tortaBZ.arrow(3.26,-0.95,1.4,0,head_width=0.02,width=0.01,head_length=0,clip_on=False,color='black',edgecolor='black')
 # coloreo las ticklabels segun categoria
 labelcol =['saddlebrown','saddlebrown','saddlebrown','saddlebrown','saddlebrown','limegreen','limegreen','limegreen','limegreen','limegreen','limegreen','limegreen','dodgerblue','grey','dodgerblue','dodgerblue','dodgerblue']
 [lab.set_color(i) for (i,lab) in zip(labelcol, histo.xaxis.get_ticklabels())] 
-fontdict = dict(color='red',weight='heavy',**csfont)
+fontdict = dict(color='k',weight='heavy',**csfont)
 tortaBZ.text(-1.5,0.7,r'BA', fontsize=22,fontdict=fontdict)
-fontdictN = dict(color='g',weight='heavy',**csfont)
+fontdictN = dict(color='k',weight='heavy',**csfont)
 tortaN.text(0.91,0.7,r'N', fontsize=22,fontdict=fontdictN)
 
 """
@@ -127,8 +129,8 @@ for i in range(1,18):
 #creo el histograma sedimentos
 ancho=0.3
 histo2 = fig.add_axes([0.18, 0.21, 0.8, 0.25])
-rect4 = histo2.bar(cols,meansedBZ[0,:],ancho, color = 'r', yerr=[np.zeros(17), meansedBZ[1,:]], error_kw=eb, linewidth=0, log=True)
-rect5 = histo2.bar(cols+ancho,meansedN[0,:], ancho, color = 'g', yerr = [np.zeros(17),meansedN[1,:]], error_kw = eb2, linewidth=0, log=True)
+rect4 = histo2.bar(cols,meansedBZ[0,:],ancho, color = 'k', yerr=[np.zeros(17), meansedBZ[1,:]], error_kw=eb, linewidth=0, log=True)
+rect5 = histo2.bar(cols+ancho,meansedN[0,:], ancho, color = 'darkgrey', yerr = [np.zeros(17),meansedN[1,:]], error_kw = eb2, linewidth=0, log=True)
 # labels y ejes
 csfont = {'fontname':'Liberation Sans'}
 histo2.set_ylabel('Sediment sterols (ug.$\mathregular{g^{-1}}$)', size=18,**csfont)
@@ -140,7 +142,17 @@ histo2.spines['top'].set_visible(False)
 histo2.spines['right'].set_visible(False)
 histo2.tick_params(length=0, which='both')
 histo2.set_xticks(cols+ancho)
-histo2.set_xticklabels(headers[1:18], rotation = 90, size=14,**csfont)
+histo2.set_xticklabels(headers[1:18], rotation = 90, position=(0,-0.03), size=14,**csfont)
+
+#clasifico rotulos histograma
+fig1=fig.add_axes([0.15,0.03,0.83,0.18],frameon=False)
+fig1.xaxis.set_visible(False)
+fig1.yaxis.set_visible(False)
+fig1.patch.set_facecolor('none')
+fig1.add_patch(Rectangle((0.02,0.19),0.277,0.78,facecolor = 'none',linewidth=1, edgecolor='k', ls='dotted',joinstyle='round'))
+fig1.text(0.05,0.06, r'Fecal sterols', fontsize=18,**csfont)
+fig1.add_patch(Rectangle((0.302,0.19),0.398,0.78,facecolor = 'none',linewidth=1, edgecolor='k', ls='dotted',joinstyle='round'))
+fig1.text(0.39,0.06, r'Plant sterols', fontsize=18,**csfont)
 
 #creo las tortas
 tortaBZ2 = fig.add_axes([0.21,0.36,0.265,0.265])
@@ -149,11 +161,15 @@ BZ2=[(np.mean(arnumsed[0:sBZ,18])),(np.mean(arnumsed[0:sBZ,19])),(np.mean(arnums
 print(BZ2)
 N2=[(np.mean(arnumsed[sBZ:(sBZ+sN),19])),(np.mean(arnumsed[sBZ:(sBZ+sN),18])),(np.mean(arnumsed[sBZ:(sBZ+sN),20])),(np.mean(arnumsed[sBZ:(sBZ+sN),21]))]
 print(N2)
-tortaBZ2.pie(BZ2,startangle=40, colors=colors,wedgeprops={'linewidth':0})
-tortaN2.pie(N2,startangle=245, colors=colorsN,wedgeprops={'linewidth':0})
+tortaBZ2.pie(BZ2,startangle=40, colors=colors,wedgeprops={'linewidth':1})
+tortaN2.pie(N2,startangle=245, colors=colorsN,wedgeprops={'linewidth':1})
 tortaBZ2.set_aspect('equal')
 tortaN2.set_aspect('equal')
 
+
+
+
+"""
 # coloreo las ticklabels segun categoria
 labelcol = ['saddlebrown','saddlebrown','saddlebrown','saddlebrown','saddlebrown','limegreen','limegreen','limegreen','limegreen','limegreen','limegreen','limegreen','grey','dodgerblue','dodgerblue','dodgerblue','dodgerblue']
 [lab.set_color(i) for (i,lab) in zip(labelcol, histo2.xaxis.get_ticklabels())] 
@@ -162,5 +178,6 @@ fontdict = dict(color='red',weight='heavy',**csfont)
 fontdictN2 = dict(color='g',weight='heavy',**csfont)
 #tortaN.text(0.91,0.3,r'N', fontsize=22,fontdict=fontdictN)
 #print(arnumfec[0,1:18])
+"""
 
 plt.show()
